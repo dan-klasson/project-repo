c40c9366a2a0e38a19a8362baee41d87ebac3044
Daniel Klasson <daniel.klasson@ipsos.com>
2020-02-11 10:30:27 +0100

    changed to e2e

--- src/project/project.resolver.spec.ts
+++ src/project/project.resolver.spec.ts
@@ -1,30 +1,68 @@
-import { GraphQLSchema } from 'graphql';
-import { buildSchema } from 'type-graphql';
 
-import { ProjectResolver } from './project.resolver';
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../app.module';
+import { ProjectModule } from './project.module'
+import { INestApplication } from '@nestjs/common';
 
-export async function generateSchema(): Promise<GraphQLSchema> {
-  try {
-    const schema = await buildSchema({
-      resolvers: [ProjectResolver],
-      validate: true,
-    });
+describe('e2e', () => {
+  let app: INestApplication;
 
-    return schema;
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
-}
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = await moduleFixture.createNestApplication().init();
+  });
 
-describe("Project", () => {
-    let schema: GraphQLSchema;
+  it('should fetch projects', async done => {
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `{
+            projects {
+              id
+              name
+            }
+          }`,
+      })
+      .expect(200, done);
+  });
 
-    beforeAll(async () => {
-        schema = await generateSchema();
-    });
-
-    it("should build schema without errors", async () => {
-        expect(schema).toBeDefined();
-    });
+  afterAll(async () => {
+    await app.close();
+  });
 });
+
+
+//import { GraphQLSchema } from 'graphql';
+//import { buildSchema } from 'type-graphql';
+//
+//import { ProjectResolver } from './project.resolver';
+//
+//export async function generateSchema(): Promise<GraphQLSchema> {
+//  try {
+//    const schema = await buildSchema({
+//      resolvers: [ProjectResolver],
+//      validate: true,
+//    });
+//
+//    return schema;
+//  } catch (e) {
+//    console.error(e);
+//    throw e;
+//  }
+//}
+//
+//describe("Project", () => {
+//    let schema: GraphQLSchema;
+//
+//    beforeAll(async () => {
+//        schema = await generateSchema();
+//    });
+//
+//    it("should build schema without errors", async () => {
+//        expect(schema).toBeDefined();
+//    });
+//});o
+//
